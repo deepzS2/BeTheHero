6610a67853f86ac3f386bc8adf83f03e72672fab
deepzS2 <alanbackman10@hotmail.com>
2020-03-25 14:53:25 -0300

    Front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Components
+import Routes from "./routes";
+
+// Global CSS
+import "./pages/global.css";
+
+// JSX (JavaScript XML)
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
